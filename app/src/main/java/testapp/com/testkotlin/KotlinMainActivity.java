3bd8f01bccf445bb6a53894bf333fb409455602b
ikyoung.kwon <ikyoung.kwon@ysmcorp.com>
2016-08-30 15:49:23 +0900

    Kotlin 시작

---
+++ app/src/main/java/testapp/com/testkotlin/KotlinMainActivity.java
@@ -0,0 +1,13 @@
+package testapp.com.testkotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KotlinMainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_kotlin_main);
+	}
+}
